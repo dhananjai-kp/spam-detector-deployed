9550186a765ee245f605d469029c518f250b378f
dhananjai-kp <dhananjaikrpandey@gmail.com>
2021-03-19 11:04:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,64 @@
+# # Importing essential libraries
+# from flask import Flask, render_template, request
+# import pickle
+# from keras.preprocessing.text import Tokenizer
+# from keras.preprocessing import sequence
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+# # filename = 'model.h5'
+# # classifier = load_model("model.h5")
+# filename = 'xg_model.pkl'
+# classifier = pickle.load(open(filename, 'rb'))
+# token = pickle.load(open('token.pkl','rb'))
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         message = request.form['message']
+#     	data = [message]
+#         token.fit_on_texts(data)
+#         sequences = token.texts_to_sequences(data)
+#         sequences_matrix = sequence.pad_sequences(sequences,maxlen=250)
+#     	my_prediction = classifier.predict(sequences_matrix)
+#     	return render_template('result.html', prediction=my_prediction)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+# #	app.debug = True
+# #	app.run(host = '0.0.0.0.', port = 5000)
+
+
+
 # Importing essential libraries
 from flask import Flask, render_template, request
 import pickle
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing import sequence
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-
-# filename = 'model.h5'
-# classifier = load_model("model.h5")
+# Load the Multinomial Naive Bayes model and CountVectorizer object from disk
 filename = 'xg_model.pkl'
 classifier = pickle.load(open(filename, 'rb'))
-token = pickle.load(open('token.pkl','rb'))
-
+cv = pickle.load(open('cv-transform.pkl','rb'))
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
     if request.method == 'POST':
-        message = request.form['message']
+    	message = request.form['message']
     	data = [message]
-        token.fit_on_texts(data)
-        sequences = token.texts_to_sequences(data)
-        sequences_matrix = sequence.pad_sequences(sequences,maxlen=250)
-    	my_prediction = classifier.predict(sequences_matrix)
+    	vect = cv.transform(data).toarray()
+    	my_prediction = classifier.predict(vect)
     	return render_template('result.html', prediction=my_prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-#	app.debug = True
-#	app.run(host = '0.0.0.0.', port = 5000)
+	app.run(debug=True)
+
